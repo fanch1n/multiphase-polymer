correct error bar range for profile averaging

analysis_md/analysis_lammps.py
@@ -118,25 +118,26 @@ def map_Mol_Sequence(atom_data, sequence_map):
     return res
 
 
-def wrap(pos, Ls):
-    ans = []
-    for i in range(len(pos)):
-        ans.append(pos[i] - np.floor(pos[i] / Ls[i]) * Ls[i])
-    return ans
-
-
-def unwrap(pos, Lz):
-    return pos - np.floor(pos / Lz) * Lz
-
-
-def unwrap(pos, Lz):
-    return pos - np.floor(pos / Lz) * Lz
-
-
-def unwrap_indx(index, mid):
-    return list(index[mid:]) + list(index[:mid])
-
-
-def get_CoM(mol_data):
-    assert np.array(mol_data).shape[1] == 3
-    return np.average(np.array(mol_data), axis=0)
+#
+# def wrap(pos, Ls):
+#    ans = []
+#    for i in range(len(pos)):
+#        ans.append(pos[i] - np.floor(pos[i] / Ls[i]) * Ls[i])
+#    return ans
+#
+#
+# def unwrap(pos, Lz):
+#    return pos - np.floor(pos / Lz) * Lz
+#
+#
+# def unwrap(pos, Lz):
+#    return pos - np.floor(pos / Lz) * Lz
+#
+#
+# def unwrap_indx(index, mid):
+#    return list(index[mid:]) + list(index[:mid])
+#
+#
+# def get_CoM(mol_data):
+#    assert np.array(mol_data).shape[1] == 3
+#    return np.average(np.array(mol_data), axis=0)

analysis_md/analysis_traj.py
@@ -4,24 +4,7 @@ from itertools import product
 import json
 import os
 from collections import Counter
-from analysis_lammps import groupby_molID, get_CoM, map_Mol_Sequence, get_SeqType
-
-
-def ref_compositions(seqs, phase_description):
-    sequences = phase_description["components"]
-    tot_compo = {x + 1: 0 for x in range(phase_description["N"])}
-    for seq in seqs.keys():
-        nchain = seqs[seq]
-        type_id = sequences[seq]
-        tot_compo[int(type_id)] += nchain
-    compo_vector = []
-    for i in sorted(tot_compo.keys()):
-        compo_vector.append(tot_compo[i])
-    return np.array(compo_vector) / sum(compo_vector)
-
-
-def density_filter(x, threshold=0.1):
-    return np.heaviside(np.array(x) - threshold, 1)
+from analysis_lammps import groupby_molID, map_Mol_Sequence, get_SeqType
 
 
 def histogram_intersection(h1, h2):
@@ -48,84 +31,6 @@ def compute_degree_of_mixing(op1, op2, density, threshold):
     f = f / np.sum(f)
     g = g / np.sum(g)
     return histogram_intersection(f, g), histogram_divergence(f, g)
-
-
-def bin_data(
-    atom_data,
-    phase_description,
-    alpha,
-    beta,
-    mol_sequence_map,
-    chain_id_divider,
-    bins,
-    Lx=20,
-):
-    """alpha beta phase coexistence"""
-    phi_alpha = ref_compositions(
-        phase_description["phases"][str(alpha)], phase_description
-    )
-    phi_beta = ref_compositions(
-        phase_description["phases"][str(beta)], phase_description
-    )
-    N, r = phase_description["N"], phase_description["r"]
-    ops = []
-    compos = []
-    rhos = []
-    for i in range(len(bins) - 1):
-        zmin, zmax = bins[i], bins[i + 1]
-        chunks = np.array(atom_data)
-        chunks = chunks[chunks[:, -1] < zmax]
-        chunks = chunks[chunks[:, -1] >= zmin]
-
-        compositions = np.zeros(N)  # chain compositions
-        chain_ids = set()
-        v0 = np.pi / 6
-        rho = v0 * len(chunks) / Lx**2 / (zmax - zmin)
-
-        for atoms in chunks:
-            chain_ids.add(atoms[1])
-        # FIXME standardize the chain type label index
-        for chain in chain_ids:
-            chain_type = mol_sequence_map[chain]  # component type: 1, ..., N
-            compositions[chain_type - 1] += 1
-
-        if np.sum(compositions) > 0.0:
-            compositions = compositions / np.sum(compositions)
-
-        # order parameter
-        if np.linalg.norm(compositions, ord=2) > 0:
-            op1 = (
-                np.dot(compositions, phi_alpha)
-                / np.linalg.norm(compositions, ord=2)
-                / np.linalg.norm(phi_alpha, ord=2)
-            )
-            op2 = (
-                np.dot(compositions, phi_beta)
-                / np.linalg.norm(compositions, ord=2)
-                / np.linalg.norm(phi_beta, ord=2)
-            )
-        else:
-            op1 = op2 = 0
-
-        # initial phase label
-        p1, p2 = 0, 0
-        for chain_id in list(chain_ids):
-            if chain_id > 2 * chain_id_divider:
-                print("incorrect phase label flag")
-                exit(1)
-            if chain_id <= chain_id_divider:
-                p1 += 1
-            else:
-                p2 += 1
-
-        ops.append([op1, op2, p1, p2, rho])
-        rhos.append(rho)
-        compos.append(compositions)
-
-    ops = np.array(ops)
-    dom = compute_degree_of_mixing(ops[:, 2], ops[:, 3], rhos, 0.1)
-
-    return ops, rhos, compos, dom
 
 
 def read_traj_file(

analysis_md/visualize_frame.py
@@ -41,25 +41,45 @@ def plot_config(atom_data, box, ax, style=None, view="vertical"):
     return
 
 
-def plot_profile(bins, profile, ax, ylabel=None, style=None, errs=[], coex_labels=[]):
+# def plot_profile(bins, profile, ax, ylabel=None, style=None, errs=[], coex_labels=[]):
+def plot_profile(
+    bins,
+    profile,
+    ax,
+    ylabel=None,
+    style=None,
+    lower_errs=[],
+    upper_errs=[],
+    coex_labels=[],
+):
     for i in range(profile.shape[1]):
         if style == "chain":
             ax.plot(bins, profile[:, i], label="chain-%d" % (1 + i))
-            if len(errs) > 0 and len(errs[:, i]) == len(profile[:, i]):
+            # if len(errs) > 0 and len(errs[:, i]) == len(profile[:, i]):
+            #    ax.fill_between(
+            #        bins,
+            #        profile[:, i] - errs[:, i],
+            #        profile[:, i] + errs[:, i],
+            #        alpha=0.5,
+            #    )
+            if len(lower_errs) > 0 and len(upper_errs) > 0:
                 ax.fill_between(
                     bins,
-                    profile[:, i] - errs[:, i],
-                    profile[:, i] + errs[:, i],
+                    lower_errs[:, i],
+                    upper_errs[:, i],
                     alpha=0.5,
                 )
+
         elif style == "op":
             if i in coex_labels:
                 ax.plot(bins, profile[:, i], label="phase-%d" % (1 + i))
-                if len(errs) > 0 and len(errs[:, i]) == len(profile[:, i]):
+                # FIXME confidence interval as error bar
+                # if len(errs) > 0 and len(errs[:, i]) == len(profile[:, i]):
+                if len(lower_errs) > 0 and len(upper_errs) > 0:
                     ax.fill_between(
                         bins,
-                        profile[:, i] - errs[:, i],
-                        profile[:, i] + errs[:, i],
+                        lower_errs[:, i],
+                        upper_errs[:, i],
                         alpha=0.5,
                     )
         else:
@@ -166,7 +186,7 @@ if __name__ == "__main__":
 
     list_compos = []
     list_ops = []
-    freq = 500
+    freq = 100
     counter = 0
     for frame in read_traj(clargs.trajpath):
         try:
@@ -191,13 +211,20 @@ if __name__ == "__main__":
             print("break at ", counter)
             break
 
+    def get_max_interval(data, p=0.95):
+        index = np.argsort(data, axis=0)
+        top_k = np.floor(data.shape[0] * 0.95)
+        return data[np.where(index == top_k)]
+
     # plotting avergage density profile and other parameter profile
     plot_profile(
         mid_bins,
         np.average(np.array(list_compos), axis=0),
         ax_chain,
         ylabel="composition",
-        errs=np.std(np.array(list_compos), axis=0),
+        # errs=np.std(np.array(list_compos), axis=0),
+        lower_errs=np.percentile(np.array(list_compos), 0.25, axis=0),
+        upper_errs=np.percentile(np.array(list_compos), 0.75, axis=0),
         style="chain",
     )
 
@@ -208,11 +235,18 @@ if __name__ == "__main__":
         ax_op,
         ylabel="order parameter",
         style="op",
-        errs=np.std(np.array(list_ops), axis=0),
+        # errs=np.std(np.array(list_ops), axis=0),
+        lower_errs=np.percentile(np.array(list_ops), 0.25, axis=0),
+        upper_errs=np.percentile(np.array(list_ops), 0.75, axis=0),
         coex_labels=[alpha - 1, beta - 1],
     )
 
-    shared = np.dot(phis[alpha - 1], phis[beta - 1])
+    shared = (
+        np.dot(phis[alpha - 1], phis[beta - 1])
+        / np.linalg.norm(phis[alpha - 1], ord=2)
+        / np.linalg.norm(phis[beta - 1], ord=2)
+    )
+
     # alpha phase is assumed to be on the left side of the box
     if clargs.plot_fitness:  # FIXME scale parameter should be set properly
         if 1e-3 < shared < 0.99:
@@ -243,8 +277,8 @@ if __name__ == "__main__":
     if clargs.output:
         if clargs.plot_fitness:
             out_config = clargs.output + "-rmse-%.3f-%.3f.png" % (left_RMSE, right_RMSE)
-        fig.savefig(out_profile, transparent=True)
-        fig0.savefig(out_config, transparent=True)
+        fig.savefig(out_profile, dpi=1200, transparent=True)
+        fig0.savefig(out_config, dpi=1200, transparent=True)
 
     ### combine plots to the make the summary plots
     if clargs.summary:
@@ -263,4 +297,4 @@ if __name__ == "__main__":
         ax1.set_axis_off()
         ax2.set_axis_off()
         fig_.tight_layout()
-        fig_.savefig(clargs.output + "summary.png")
+        fig_.savefig(clargs.output + "-summary.png", dpi=1200, transparent=True)